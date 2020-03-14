0ab320a18e433a92fb43733831d88a1632689b71
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2020-03-13 22:11:57 -0600

    Duplex y Transforms streams

---
+++ learning-node/streams/transform.js
@@ -0,0 +1,12 @@
+const {
+    Transform
+} = require("stream");
+
+const transformStream = new Transform({
+    transform(chunk, enconding, callback) {
+        this.push(chunk.toString().toUpperCase());
+        callback();
+    },
+});
+
+process.stdin.pipe(transformStream).pipe(process.stdout);

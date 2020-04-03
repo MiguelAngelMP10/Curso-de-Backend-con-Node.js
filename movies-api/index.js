e216495450348c624a3a0a0e53cb6fec124cca86
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2020-04-02 23:03:52 -0600

    Implementando un CRUD en Express.js

--- movies-api/index.js
+++ movies-api/index.js
@@ -4,15 +4,9 @@ const app =  express();
 const {config} =  require('./config/index');
 
 
+const moviesApi = require('./routes/movies');
 
-app.get('/', (req, res) => {
-    res.send('Hola Mundo');
-});
-
-app.get('/json', (req, res) => {
-    res.json({mensaje:'Hola mundo'})
-});
-
+moviesApi(app);
 
 app.listen(config.port, () => {
     console.log(`App listening on port ${config.port}! `);

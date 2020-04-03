349af0c512b1f77cb18e705e54ce79b1fbaef758
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2020-04-02 23:39:28 -0600

    Métodos idempotentes del CRUD

--- movies-api/routes/movies.js
+++ movies-api/routes/movies.js
@@ -9,6 +9,57 @@ function moviesApi(app) {
 
     app.use('/api/movies', router);
 
+    router.get("/:movieId", async function (req, res, next) {
+        try {
+            const movie = await Promise.resolve(moviesMock[0]);
+            res.status(200).json({
+                data: movie,
+                menssage: 'Movie retrieved',
+            })
+        } catch (error) {
+            next(error)
+        }
+    });
+
+
+    router.post("/", async function (req, res, next) {
+        try {
+            const createdMovieId = await Promise.resolve(moviesMock[0].id);
+            res.status(201).json({
+                data: createdMovieId,
+                menssage: 'Movie created',
+            })
+        } catch (error) {
+            next(error)
+        }
+    });
+
+
+    router.put("/:moviId", async function (req, res, next) {
+        try {
+            const updtatedMovieId = await Promise.resolve(moviesMock[0].id);
+            res.status(200).json({
+                data: updtatedMovieId,
+                menssage: 'Movie updtated',
+            })
+        } catch (error) {
+            next(error)
+        }
+    });
+
+    router.delete("/:moviId", async function (req, res, next) {
+        try {
+            const deletedMovieId = await Promise.resolve(moviesMock[0].id);
+            res.status(200).json({
+                data: deletedMovieId,
+                menssage: 'Movie deleted',
+            })
+        } catch (error) {
+            next(error)
+        }
+    });
+
+
     router.get("/", async function (req, res, next) {
         try {
             const movies = await Promise.resolve(moviesMock);
@@ -24,3 +75,5 @@ function moviesApi(app) {
 
 
 module.exports = moviesApi;
+
+

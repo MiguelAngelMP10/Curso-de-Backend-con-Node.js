ec9232da775d0e56ad32a0edbaa07d571277d29a
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2020-04-29 22:25:38 -0500

    ¿Cómo implementar una capa de manejo de caché?

--- movies-api/routes/movies.js
+++ movies-api/routes/movies.js
@@ -9,6 +9,12 @@ const {
 
 const validationHandler = require('../utils/middleware/validationHandler');
 
+const cacheResponse = require('../utils/cacheResponse');
+const {
+  FIVE_MINUTES_IN_SECONDS,
+  SIXTY_MINUTES_IN_SECONDS,
+} = require('../utils/time');
+
 function moviesApi(app) {
   const router = express.Router();
 
@@ -17,6 +23,8 @@ function moviesApi(app) {
   const moviesService = new MovieService();
 
   router.get('/', async function (req, res, next) {
+    cacheResponse(res, FIVE_MINUTES_IN_SECONDS);
+
     const { tags } = req.query;
     try {
       const movies = await moviesService.getMovies({ tags });
@@ -34,6 +42,7 @@ function moviesApi(app) {
     '/:movieId',
     validationHandler({ movieId: movieIdSchema }, 'params'),
     async function (req, res, next) {
+      cacheResponse(res, SIXTY_MINUTES_IN_SECONDS);
       const { movieId } = req.params;
 
       try {
